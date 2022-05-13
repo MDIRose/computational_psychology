d8758e8ea34f7d88b852ac1f8adc59f747c1c029
RealJasomo <cramerj1@rose-hulman.edu>
2022-05-13 14:26:35 -0400

    Fixed multiple_regions with max instead of sum

--- code/multiple_positive_regions_model.py
+++ code/multiple_positive_regions_model.py
@@ -3,6 +3,7 @@ from utility_functions import split_number_regions
 
 from typing import List
 
+
 def generalize(consequential_region: int, positive_samples: List[int],  n_region: int) -> List[float]:
     """
     :param consequential_region: size of consequential region
@@ -10,9 +11,11 @@ def generalize(consequential_region: int, positive_samples: List[int],  n_region
     :param n_regions: split into n_regions
     :return: list of posterior probabilities for each discrete value in consequential region
     """
-    regions = split_number_regions(consequential_region, positive_samples, n_region)
+    regions = split_number_regions(
+        consequential_region, positive_samples, n_region)
     positive_prob = baseline.generalize(consequential_region, [], [])
     for region in regions:
         prob = baseline.generalize(consequential_region, region, [])
-        positive_prob = [positive_prob[i] + prob[i] for i in range(len(positive_prob))]
+        positive_prob = [max(positive_prob[i], prob[i])
+                         for i in range(len(positive_prob))]
     return positive_prob
